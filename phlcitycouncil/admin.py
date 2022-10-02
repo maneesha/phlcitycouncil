93d3948b04addf72466143278558b7d2a7a36ef3
maneesha <ahseenam@gmail.com>
2022-10-02 12:22:05 -0400

    update admin view for Person

--- phlcitycouncil/admin.py
+++ phlcitycouncil/admin.py
@@ -4,8 +4,18 @@ from django.contrib import admin
 
 from .models import Person, Seat, Election, Candidate, Term
 
-admin.site.register(Person)
+# admin.site.register(Person)
 admin.site.register(Seat)
 admin.site.register(Election)
 admin.site.register(Candidate)
 admin.site.register(Term)
+
+class CandidateInline(admin.TabularInline):
+    model = Candidate
+    extra = 0
+
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name', 'birth', 'death')
+    fields = ['first_name', 'last_name', ('birth', 'death')]
+    inlines = [CandidateInline]
