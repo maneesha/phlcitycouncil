bbaf7dd0c5da6893dd42d33de13ca8d30d0951ac
maneesha <ahseenam@gmail.com>
2022-10-02 09:58:39 -0400

    add urls to project

--- citycouncil/urls.py
+++ citycouncil/urls.py
@@ -19,3 +19,22 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+from django.urls import include 
+
+urlpatterns += [
+    path('phlcitycouncil/', include('phlcitycouncil.urls'))
+]
+
+from django.views.generic import RedirectView
+
+urlpatterns += [
+    path('', RedirectView.as_view(url="phlcitycouncil", permanent=True))
+]
+
+
+# Use static() to add URL mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
