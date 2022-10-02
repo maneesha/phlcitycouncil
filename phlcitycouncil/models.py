3d4689463f60ece6b49355188068a1885e9327b2
maneesha <ahseenam@gmail.com>
2022-10-02 10:16:08 -0400

    add dates for person model

--- phlcitycouncil/models.py
+++ phlcitycouncil/models.py
@@ -7,6 +7,9 @@ class Person(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
+    birth = models.DateField()
+    death = models.DateField()
+
     def __str__(self):
         return self.first_name +  " " + self.last_name
 
