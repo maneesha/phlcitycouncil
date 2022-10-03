7d394822bf522c605d95381fc5d5c31564b56db1
maneesha <ahseenam@gmail.com>
2022-10-02 21:09:43 -0400

    create sample index view

--- phlcitycouncil/views.py
+++ phlcitycouncil/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
+from .models import Person, Seat
+
 # Create your views here.
+
+def index(request):
+
+    """ View function for home page """
+
+    num_persons = Person.objects.all().count()
+    num_seats = Seat.objects.all().count()
+
+    context = {
+        'num_persons':num_persons,
+        'num_seats':num_seats,
+    }
+
+    return render(request, 'index.html', context=context)
+
+    
