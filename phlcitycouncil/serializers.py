b6445d12f61dc489cc12d6a8eb8baa5a3aa7f1aa
maneesha <ahseenam@gmail.com>
2022-11-04 15:13:35 -0400

    add seat to election serializer

--- phlcitycouncil/serializers.py
+++ phlcitycouncil/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
-from .models import Election, Candidate, Person 
+from .models import Election, Candidate, Person, Seat
+
+class SeatSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Seat
+        fields = ('seat_name')
 
 class ElectionSerializer(serializers.ModelSerializer):
 
+    seat = SeatSerializer(many = True, read_only=True)
+
     class Meta:
         model = Election
-
-        fields = ('election_date', 'election_type', 'election_seat')
+        depth = 1
+        fields = ('election_date', 'election_type', 'election_seat', 'seat',)
 
