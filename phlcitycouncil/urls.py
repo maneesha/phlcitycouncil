0eb5463a8903b24c07ba08e1fdb9c38d5478384a
maneesha <ahseenam@gmail.com>
2022-11-04 16:43:52 -0400

    add names to all api urls

--- phlcitycouncil/urls.py
+++ phlcitycouncil/urls.py
@@ -8,11 +8,12 @@ urlpatterns = [
     path('election/', views.ElectionView.as_view(), name = 'election' ),
     path('election/<int:pk>', views.ElectionDetailView.as_view(), name = "election-detail"),
     path('term/', views.TermView.as_view(), name="term"),
-    path('api/election/', views.ElectionListAPI.as_view()),
-    path('api/election/<int:pk>/', views.ElectionDetailAPI.as_view()),
-    path('api/person/', views.PersonListAPI.as_view()),
-    path('api/person/<int:pk>', views.PersonDetailAPI.as_view()),
-    path('api/candidate/', views.CandidateListAPI.as_view()),
-    path('api/candidate/<int:pk>', views.CandidateDetailAPI.as_view())
+    path('api/election/', views.ElectionListAPI.as_view(), name='election_list_api'),
+    path('api/election/<int:pk>/', views.ElectionDetailAPI.as_view(), name='election_detail_api'),
+    path('api/person/', views.PersonListAPI.as_view(), name='person_list_api'),
+    path('api/person/<int:pk>', views.PersonDetailAPI.as_view(), name='person_detail_api'),
+    path('api/candidate/', views.CandidateListAPI.as_view(), name='candidate_list_api'),
+    path('api/candidate/<int:pk>', views.CandidateDetailAPI.as_view(), name='candidate_detail_api'),
+    path('api/', views.api_root)
 
 ]
