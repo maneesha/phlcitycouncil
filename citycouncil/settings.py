6dab3d526bcb3133637a87be4e4acdb10b9b585d
maneesha <ahseenam@gmail.com>
2022-11-05 17:17:44 -0400

    update env var calls for secret key & debug

--- citycouncil/settings.py
+++ citycouncil/settings.py
@@ -21,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+# SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ['DJANGO_DEBUG']
+# DEBUG = os.environ['DJANGO_DEBUG']
+DEBUG = os.getenv('DJANGO_DEBUG')
 
 ALLOWED_HOSTS = ['127.0.0.1', 'citycouncilphl.pythonanywhere.com']
 
